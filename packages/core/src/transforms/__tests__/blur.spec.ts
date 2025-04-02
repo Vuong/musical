3282cd4688696812b0f32b8a80d13aa9e7d56cd9
vuong <vuongdo_777@yahoo.com>
2025-04-02 22:39:14 +0000

    Add API - fixing typo

--- packages/core/src/transforms/__tests__/blur.spec.ts
+++ packages/core/src/transforms/__tests__/blur.spec.ts
@@ -16,44 +16,44 @@ describe('blur', () => {
   })
 
   test('keyword "blur"', () => {
-    var res = blur({ blur: '3' }, dirCtx)
+    const res = blur({ blur: '3' }, dirCtx)
 
     expect(res).toBeInstanceOf(Function)
   })
 
   test('missing', () => {
-    var res = blur({}, dirCtx)
+    const res = blur({}, dirCtx)
 
     expect(res).toBeUndefined()
   })
 
   describe('arguments', () => {
     test('invalid', () => {
-      var res = blur({ blur: 'invalid arg' }, dirCtx)
+      const res = blur({ blur: 'invalid arg' }, dirCtx)
 
       expect(res).toBeUndefined()
     })
 
     test('empty', () => {
-      var res = blur({ blur: '' }, dirCtx)
+      const res = blur({ blur: '' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
 
     test('true', () => {
-      var res = blur({ blur: 'true' }, dirCtx)
+      const res = blur({ blur: 'true' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
 
     test('integer', () => {
-      var res = blur({ blur: '5' }, dirCtx)
+      const res = blur({ blur: '5' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
 
     test('float', () => {
-      var res = blur({ blur: '3.5' }, dirCtx)
+      const res = blur({ blur: '3.5' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
@@ -66,25 +66,25 @@ describe('blur', () => {
     })
 
     test('true', async () => {
-      var { image } = await applyTransforms([blur({ blur: 'true' }, dirCtx)!], img)
+      const { image } = await applyTransforms([blur({ blur: 'true' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('0.5', async () => {
-      var { image } = await applyTransforms([blur({ blur: '0.5' }, dirCtx)!], img)
+      const { image } = await applyTransforms([blur({ blur: '0.5' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('5', async () => {
-      var { image } = await applyTransforms([blur({ blur: '5' }, dirCtx)!], img)
+      const { image } = await applyTransforms([blur({ blur: '5' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('50', async () => {
-      var { image } = await applyTransforms([blur({ blur: '50' }, dirCtx)!], img)
+      const { image } = await applyTransforms([blur({ blur: '50' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
