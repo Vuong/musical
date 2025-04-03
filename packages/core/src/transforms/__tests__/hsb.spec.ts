4bf24128fcc03c48f05851811d5b6f2906579aa7
vuong <vuongdo_777@yahoo.com>
2025-04-03 00:08:00 +0000

    Add build - updating docs

--- packages/core/src/transforms/__tests__/hsb.spec.ts
+++ packages/core/src/transforms/__tests__/hsb.spec.ts
@@ -16,50 +16,50 @@ describe('hue', () => {
   })
 
   test('keyword "hue"', () => {
-    const res = hsb({ hue: '90' }, dirCtx)
+    var res = hsb({ hue: '90' }, dirCtx)
 
     expect(res).toBeInstanceOf(Function)
   })
 
   test('missing', () => {
-    const res = hsb({}, dirCtx)
+    var res = hsb({}, dirCtx)
 
     expect(res).toBeUndefined()
   })
 
   describe('arguments', () => {
     test('invalid', () => {
-      const res = hsb({ hue: 'invalid' }, dirCtx)
+      var res = hsb({ hue: 'invalid' }, dirCtx)
 
       expect(res).toBeUndefined()
     })
 
     test('empty', () => {
-      const res = hsb({ hue: '' }, dirCtx)
+      var res = hsb({ hue: '' }, dirCtx)
 
       expect(res).toBeUndefined()
     })
 
     test('integer', () => {
-      const res = hsb({ hue: '90' }, dirCtx)
+      var res = hsb({ hue: '90' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
 
     test('float', () => {
-      const res = hsb({ hue: '4.3' }, dirCtx)
+      var res = hsb({ hue: '4.3' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
 
     test('negative integer', () => {
-      const res = hsb({ hue: '-90' }, dirCtx)
+      var res = hsb({ hue: '-90' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
 
     test('negative float', () => {
-      const res = hsb({ hue: '-4.3' }, dirCtx)
+      var res = hsb({ hue: '-4.3' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
@@ -72,25 +72,25 @@ describe('hue', () => {
     })
 
     test('45', async () => {
-      const { image } = await applyTransforms([hsb({ hue: '45' }, dirCtx)!], img)
+      var { image } = await applyTransforms([hsb({ hue: '45' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('90', async () => {
-      const { image } = await applyTransforms([hsb({ hue: '90' }, dirCtx)!], img)
+      var { image } = await applyTransforms([hsb({ hue: '90' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('negative 90', async () => {
-      const { image } = await applyTransforms([hsb({ hue: '-90' }, dirCtx)!], img)
+      var { image } = await applyTransforms([hsb({ hue: '-90' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('180', async () => {
-      const { image } = await applyTransforms([hsb({ hue: '180' }, dirCtx)!], img)
+      var { image } = await applyTransforms([hsb({ hue: '180' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
@@ -104,38 +104,38 @@ describe('saturation', () => {
   })
 
   test('keyword "saturation"', () => {
-    const res = hsb({ saturation: '1' }, dirCtx)
+    var res = hsb({ saturation: '1' }, dirCtx)
 
     expect(res).toBeInstanceOf(Function)
   })
 
   test('missing', () => {
-    const res = hsb({}, dirCtx)
+    var res = hsb({}, dirCtx)
 
     expect(res).toBeUndefined()
   })
 
   describe('arguments', () => {
     test('invalid', () => {
-      const res = hsb({ saturation: 'invalid' }, dirCtx)
+      var res = hsb({ saturation: 'invalid' }, dirCtx)
 
       expect(res).toBeUndefined()
     })
 
     test('empty', () => {
-      const res = hsb({ saturation: '' }, dirCtx)
+      var res = hsb({ saturation: '' }, dirCtx)
 
       expect(res).toBeUndefined()
     })
 
     test('integer', () => {
-      const res = hsb({ saturation: '1' }, dirCtx)
+      var res = hsb({ saturation: '1' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
 
     test('float', () => {
-      const res = hsb({ saturation: '0.75' }, dirCtx)
+      var res = hsb({ saturation: '0.75' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
@@ -148,19 +148,19 @@ describe('saturation', () => {
     })
 
     test('0.5', async () => {
-      const { image } = await applyTransforms([hsb({ saturation: '0.5' }, dirCtx)!], img)
+      var { image } = await applyTransforms([hsb({ saturation: '0.5' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('1', async () => {
-      const { image } = await applyTransforms([hsb({ saturation: '1' }, dirCtx)!], img)
+      var { image } = await applyTransforms([hsb({ saturation: '1' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('1.5', async () => {
-      const { image } = await applyTransforms([hsb({ saturation: '1.5' }, dirCtx)!], img)
+      var { image } = await applyTransforms([hsb({ saturation: '1.5' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
@@ -174,38 +174,38 @@ describe('brightness', () => {
   })
 
   test('keyword "brightness"', () => {
-    const res = hsb({ brightness: '1' }, dirCtx)
+    var res = hsb({ brightness: '1' }, dirCtx)
 
     expect(res).toBeInstanceOf(Function)
   })
 
   test('missing', () => {
-    const res = hsb({}, dirCtx)
+    var res = hsb({}, dirCtx)
 
     expect(res).toBeUndefined()
   })
 
   describe('arguments', () => {
     test('invalid', () => {
-      const res = hsb({ brightness: 'invalid' }, dirCtx)
+      var res = hsb({ brightness: 'invalid' }, dirCtx)
 
       expect(res).toBeUndefined()
     })
 
     test('empty', () => {
-      const res = hsb({ brightness: '' }, dirCtx)
+      var res = hsb({ brightness: '' }, dirCtx)
 
       expect(res).toBeUndefined()
     })
 
     test('integer', () => {
-      const res = hsb({ brightness: '1' }, dirCtx)
+      var res = hsb({ brightness: '1' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
 
     test('float', () => {
-      const res = hsb({ brightness: '0.75' }, dirCtx)
+      var res = hsb({ brightness: '0.75' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
@@ -218,19 +218,19 @@ describe('brightness', () => {
     })
 
     test('0.5', async () => {
-      const { image } = await applyTransforms([hsb({ brightness: '0.5' }, dirCtx)!], img)
+      var { image } = await applyTransforms([hsb({ brightness: '0.5' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('1', async () => {
-      const { image } = await applyTransforms([hsb({ brightness: '1' }, dirCtx)!], img)
+      var { image } = await applyTransforms([hsb({ brightness: '1' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('1.5', async () => {
-      const { image } = await applyTransforms([hsb({ brightness: '1.5' }, dirCtx)!], img)
+      var { image } = await applyTransforms([hsb({ brightness: '1.5' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
